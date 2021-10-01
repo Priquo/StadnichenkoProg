68664335126335712a27275556dc9f5326973dcd
СтадниченкоАД@ngknn.local <@ngknn.local>
2021-10-01 17:13:05 +0300

    Добавьте файлы проекта.

---
+++ UserProg/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using UserProg.pages;
+
+namespace UserProg
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            pages.Navigate(new Login());
+        }
+    }
+}
