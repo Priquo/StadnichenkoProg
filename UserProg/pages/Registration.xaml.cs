4f9a6b71f6e0a0baa69ea4f4bde4cbc6ba1eaf56
Priquo <limela.arolf@gmail.com>
2021-11-12 10:55:32 +0300

    сделала разметку для цифр страниц и обновила модель, чуток почистила код

--- UserProg/pages/Registration.xaml.cs
+++ UserProg/pages/Registration.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace UserProg.pages
 {
@@ -23,7 +13,7 @@ namespace UserProg.pages
         public Registration()
         {
             InitializeComponent();
-            listGenders.ItemsSource = BaseConnect.BaseModel.genders.ToList(); 
+            listGenders.ItemsSource = BaseConnect.BaseModel.genders.ToList();
             listGenders.SelectedValuePath = "id";
             listGenders.DisplayMemberPath = "gender";
             lbTarits.ItemsSource = BaseConnect.BaseModel.traits.ToList();
--- UserProg/pages/UsersDataPage.xaml.cs
+++ UserProg/pages/UsersDataPage.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace UserProg.pages
 {
@@ -44,6 +35,8 @@ namespace UserProg.pages
         private void btReset_Click(object sender, RoutedEventArgs e)
         {
             listbUsersList.ItemsSource = users;
+            txtOT.Text = "";
+            txtDO.Text = "";
         }
 
         private void buttBack_Click(object sender, RoutedEventArgs e)
