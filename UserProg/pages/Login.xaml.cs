6b0a6fef0740cde56ea2262d5a11b94e0c21c731
Пыш Пыш <limela.arolf@gmail.com>
2021-12-15 16:49:54 +0300

    генерация кодк

--- UserProg/pages/Login.xaml.cs
+++ UserProg/pages/Login.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Threading;
 
 namespace UserProg.pages
 {
@@ -13,11 +15,38 @@ namespace UserProg.pages
         {
             InitializeComponent();
         }
+        string kode;
+        bool flagKode = false;
         private void buttonReg_Click(object Sender, RoutedEventArgs e)
         {
             LoadPages.MainFrame.Navigate(new Registration());
         }
+        Random random = new Random();
+        DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        private void generateKey()
+        {
+            buttResetCod.IsEnabled = false;
+            kode = "";
 
+            for (int i = 0; i < 8; i++)
+            {
+                kode += ((char)random.Next(33, 122)).ToString();
+            }
+            tboxCod.Text = kode;
+            MessageBox.Show(kode, "введите код в соответствующее поле на форме.", MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.OK, MessageBoxOptions.RightAlign);            
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 10);
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Start();
+        }
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            buttResetCod.IsEnabled = true;
+            for (int i = 0; i < 8; i++)
+            {
+                kode += ((char)random.Next(33, 122)).ToString();
+            }
+            MessageBox.Show("время вышло");
+        }
         private void buttonLogin_Click(object sender, RoutedEventArgs e)
         {
             try
@@ -25,17 +54,31 @@ namespace UserProg.pages
                 auth CurrUser = BaseConnect.BaseModel.auth.FirstOrDefault(x => x.login == textboxLogin.Text && x.password == psswBox1.Password);
                 if (CurrUser != null)
                 {
-                    switch (CurrUser.role)
+                    if (flagKode == false)
                     {
-                        case 1:
-                            MessageBox.Show("Вы вошли как крутой админ");
-                            LoadPages.MainFrame.Navigate(new AdminPage());
-                            break;
-                        case 2:
-                        default:
-                            MessageBox.Show("Вы вошли как непримечательный пользователь");
-                            LoadPages.MainFrame.Navigate(new Form(CurrUser));
-                            break;
+                        generateKey();
+                        flagKode = true;
+                    }
+                    else if (tboxCod.Text == kode)
+                    {
+                        switch (CurrUser.role)
+                        {
+                            case 1:
+                                MessageBox.Show("Вы вошли как крутой админ");
+                                LoadPages.MainFrame.Navigate(new AdminPage());
+                                break;
+                            case 2:
+                            default:
+                                MessageBox.Show("Вы вошли как непримечательный пользователь");
+                                LoadPages.MainFrame.Navigate(new Form(CurrUser));
+                                break;
+                        }
+                        dispatcherTimer.Stop();
+                        tboxCod.Text = "";
+                    }         
+                    else
+                    {
+                        MessageBox.Show("Неверный код");
                     }
                 }
                 else
@@ -45,6 +88,12 @@ namespace UserProg.pages
             {
                 MessageBox.Show("Невероятно, но что-то создает ошибку, программа не работает");
             }
+        }
+
+        private void buttResetCod_Click(object sender, RoutedEventArgs e)
+        {
+            generateKey();
+            flagKode = true;
         }
     }
 }
